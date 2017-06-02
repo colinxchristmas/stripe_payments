22725b8751ca863dd8d386b52831ab953ea3ff4f
Colin Christmas <colindchristmas@gmail.com>
2017-06-02 01:34:01 -0700

    added more devise methods to user added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :first_name, :last_name, presence: true
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable, password_length: 8..128
 
   def username
     [first_name, last_name].join (' ')
