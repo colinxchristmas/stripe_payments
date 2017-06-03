ddbce5bb3df5c790b8823991582ddc04a7bc9ef0
Colin Christmas <colindchristmas@gmail.com>
2017-06-03 01:09:36 -0700

    updated alerts to bootstrap updated crud forms and pages for products updated navigation added helpers for pricing added fontawesome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
+  def formatted_price(amount)
+    sprintf("$%0.2f", amount / 100.0)
+  end
+
   private
-  
+
   def custom_bootstrap_flash(flash_type)
     case flash_type
       when "success"
