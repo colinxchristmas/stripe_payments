467f97a07df4aafac40acf57cccd9936a595f8ab
Colin Christmas <colindchristmas@gmail.com>
2017-06-09 23:47:25 -0700

    removed erroneous comments from factory spec

--- test/factories/subscriptions.rb
+++ test/factories/subscriptions.rb
@@ -1,10 +1,3 @@
-# FactoryGirl.define do
-#   factory :subscription do
-#     stripe_id "MyString"
-#     user nil
-#     plan nil
-#   end
-# end
 FactoryGirl.define do
   factory :subscription do
     stripe_id          ['sub_', Faker::Number.hexadecimal(10)].join('')
