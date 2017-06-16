74cf8ce50e6c5d715b4eea4de8a2281f57b0c34d
Colin Christmas <colindchristmas@gmail.com>
2017-06-16 02:59:37 -0700

    added stripe mock funtionality for transactions controller added database cleaner to controller tests added card method to add proper card field to card object before test starts one test failing for card deletion due to address foreign key fix next go

--- test/factories/cards.rb
+++ test/factories/cards.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :card do
-    stripe_id      ['t_card_', Faker::Number.hexadecimal(20)].join('')
+    stripe_id ['t_card_', Faker::Number.hexadecimal(20)].join('')
+
     card_name      Faker::Name.name
     card_last_four Faker::Number.between(1000, 9999)
     card_type      "Visa"
@@ -18,6 +19,10 @@ FactoryGirl.define do
     association :user, factory: :user
     after(:create) do |card|
       create(:address, card: card)
+    end
+
+    after(:build) do |card|
+      build(:address, card: card)
     end
   end
 end
